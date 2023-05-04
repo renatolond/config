cb0ca5bf33f95cc7a3fe37d852d096856fa200bb
Renato "Lond" Cerqueira <renato@lond.com.br>
2023-05-04 14:08:20 +0200

    Adjust cmp mappings for tab and shift+tab

--- neovim/cmp.lua
+++ neovim/cmp.lua
@@ -4,6 +4,13 @@ cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
 -- Set up nvim-cmp.
 local cmp = require'cmp'
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local luasnip = require("luasnip")
 
 -- Requires "nerd" fonts: https://www.nerdfonts.com/font-downloads
 local kind_icons = {
@@ -66,6 +73,29 @@ mapping = cmp.mapping.preset.insert({
   ['<C-Space>'] = cmp.mapping.complete(),
   ['<C-e>'] = cmp.mapping.abort(),
   ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  ["<Tab>"] = cmp.mapping(function(fallback)
+    if cmp.visible() then
+      cmp.select_next_item()
+      -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+      -- they way you will only jump inside the snippet region
+    elseif luasnip.expand_or_jumpable() then
+      luasnip.expand_or_jump()
+    elseif has_words_before() then
+      cmp.complete()
+    else
+      fallback()
+    end
+  end, { "i", "s" }),
+
+  ["<S-Tab>"] = cmp.mapping(function(fallback)
+    if cmp.visible() then
+      cmp.select_prev_item()
+    elseif luasnip.jumpable(-1) then
+      luasnip.jump(-1)
+    else
+      fallback()
+    end
+  end, { "i", "s" }),
 }),
 sources = cmp.config.sources({
   { name = 'nvim_lsp' },
